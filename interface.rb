f8a741c1198d44a10eb9cad58c16cbe48bda86f2
lizzerdrix <liz@lizbaillie.com>
2014-10-08 13:41:22 -0400

    works with single digits only, with error message for larger nums. Still need to add functionality for larger nums

--- interface.rb
+++ interface.rb
@@ -10,8 +10,14 @@ class NumberFinder
   end
 
   def get_num_fact(doc,num)
-    File.readlines(doc).each do |line|
-      match_num(line,num)
+    numbers = ["1","2","3","4","5","6","7","8","9","0"]
+    if numbers.include?(num)
+      File.readlines(doc).each do |line|
+        match_num(line,num)
+      end
+    else
+      puts "Sorry! You may only use a single-digit number at this time. Try again!"
+      NumberFinder.new
     end
   end
 
@@ -21,6 +27,7 @@ class NumberFinder
         next
       elsif char == num[ind] && line[ind+1] == " "
         puts line
+        puts "I hope you enjoyed learning more about your favorite number!"
       end
     end
   end
