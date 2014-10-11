b87455bf7e6ced8f261766744b1cbbb21c43adcc
lizzerdrix <liz@lizbaillie.com>
2014-10-10 22:12:37 -0400

    got match_num function to work for all number between 1 and 9999 except for those not in the original document

--- interface.rb
+++ interface.rb
@@ -5,31 +5,29 @@ class NumberFinder
   def initialize
     puts "Enter your favorite number: "
     number = gets.chomp
-    #binding.pry
     get_num_fact("numbers.txt",number)
+    puts "Do you want to get information on another number? (Y/N)"
+    choice = gets.chomp
+    if choice.upcase == 'Y' || choice.upcase == 'YES'
+      NumberFinder.new
+    else
+      puts "Have a nice day! Goodbye."
+    end
   end
 
   def get_num_fact(doc,num)
-    numbers = ["1","2","3","4","5","6","7","8","9","0"]
-    if numbers.include?(num)
-      File.readlines(doc).each do |line|
-        match_num(line,num)
-      end
-    else
-      puts "Sorry! You may only use a single-digit number at this time. Try again!"
-      NumberFinder.new
+    File.readlines(doc).each do |line|
+      match_num(line,num)
     end
   end
 
   def match_num(line, num)
-    line.split("").each_with_index do |char, ind|
-      if ind > 3
-        next
-      elsif char == num[ind] && line[ind+1] == " "
+    line_array = line.split(" ")
+    line_array.each_with_index do |char, ind|
+      if ind == 0 && char == num
         puts line
         puts "I hope you enjoyed learning more about your favorite number!"
       end
-      #need to iterate over every number between 1 and 9999 and match to input
     end
   end
 
